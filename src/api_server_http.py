7b0c7a852a0ea1197ac38d7ff1371c325e1ded3c
Venkata Abhiram Karuturi <127250133+abhiram-999@users.noreply.github.com>
2025-11-06 18:46:49 -0500

    Update api_server_http.py

--- src/api_server_http.py
+++ src/api_server_http.py
@@ -1,7 +1,6 @@
 import uvicorn
-from .main import app
-from .utils.config import HOST, PORT
+from src.main import app
+from src.utils.config import HOST, PORT
 
 if __name__ == "__main__":
     uvicorn.run(app, host=HOST, port=PORT)
-

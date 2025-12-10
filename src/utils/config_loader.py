9e4a1006d8ac0767f48fe0dcc761646e5f66e7ec
snarahagmu11 <snaraha@gmu.edu>
2025-12-09 20:42:04 -0500

    Update config_loader.py

--- src/utils/config_loader.py
+++ src/utils/config_loader.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import yaml
 
 def load_config(path: str = "config/settings.yaml"):
-    # Point to project root config folder
     cfg_path = Path(__file__).resolve().parents[2] / "config" / "settings.yaml"
     if not cfg_path.exists():
         raise FileNotFoundError(f"Config not found at: {cfg_path}")
